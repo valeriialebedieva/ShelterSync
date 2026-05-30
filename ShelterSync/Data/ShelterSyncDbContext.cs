6dd568f0aa348c84a2e1c34aeb43c32e80e7aa5d
valeriialebedieva <valeriia.lebedieva1@gmail.com>
2026-05-30 16:36:16 +0200

    Create Admin and Pet Management UI pages

--- ShelterSync/Data/ShelterSyncDbContext.cs
+++ ShelterSync/Data/ShelterSyncDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using ShelterSync.Models;
 
 namespace ShelterSync.Data;
 
@@ -8,8 +9,10 @@ namespace ShelterSync.Data;
 /// </summary>
 public class ShelterSyncDbContext : DbContext
 {
-    public ShelterSyncDbContext(DbContextOptions<ShdoelterSyncDbContext> options)
+    public ShelterSyncDbContext(DbContextOptions<ShelterSyncDbContext> options)
         : base(options) { }
+
+    public DbSet<Pet> Pets { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
